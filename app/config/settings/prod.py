f67b06ec77d7d6a698684985372e957e47827dd4
JangasCodingplace <janisgoesser92@gmail.com>
2022-12-10 22:19:45 +0100

    INIT-7 bugfix on AWS reference

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -57,7 +57,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_DEFAULT_ACL = 'public-read'
 AWS_QUERYSTRING_AUTH = False
-AWS_STATIC_FILE_LOCATION = "staticfiles/"
+AWS_LOCATION = "staticfiles/"
 AWS_MEDIA_FILE_LOCATION = "mediafiles/"
 
 # STATIC
@@ -65,7 +65,7 @@ AWS_MEDIA_FILE_LOCATION = "mediafiles/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [BASE_DIR / "staticfiles" / "react" / "build" / "static"]  # noqa
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_STATIC_FILE_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
