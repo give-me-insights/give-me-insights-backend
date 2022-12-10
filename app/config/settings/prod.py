94c18337b5045aa81c4b137a3249aff21ca76ca6
JangasCodingplace <janisgoesser92@gmail.com>
2022-12-10 21:53:59 +0100

    INIT-12 add Media to s3 bucket

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -57,13 +57,18 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_DEFAULT_ACL = 'public-read'
 AWS_QUERYSTRING_AUTH = False
-AWS_LOCATION = "staticfiles/"
+AWS_STATIC_FILE_LOCATION = "staticfiles/"
+AWS_MEDIA_FILE_LOCATION = "mediafiles/"
 
 # STATIC
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [BASE_DIR / "staticfiles" / "react" / "build" / "static"]  # noqa
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_STATIC_FILE_LOCATION)
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# MEDIA
+# ------------------------------------------------------------------------------
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_MEDIA_FILE_LOCATION)
