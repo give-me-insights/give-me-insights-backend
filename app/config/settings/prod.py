cd6bdfe79c0444abe9d196fdc6331e1a297a2b08
JangasCodingplace <janisgoesser92@gmail.com>
2022-12-10 22:08:12 +0100

    INIT-7 add placeholder to templates directory

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -71,4 +71,4 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # MEDIA
 # ------------------------------------------------------------------------------
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_MEDIA_FILE_LOCATION)
+# MEDIA_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_MEDIA_FILE_LOCATION)
