988e2e924af384ffa41170a2a93b5aa82b952368
JangasCodingplace <janisgoesser92@gmail.com>
2022-12-11 03:04:40 +0100

    INIT-8 bugfix remove media URL

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -70,4 +70,4 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # MEDIA
 # ------------------------------------------------------------------------------
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+# MEDIA_URL = "https://%s/%s/" % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
